e1e1b58b6c22229c9a396e624f35b1699e599a0d
nishantcoder97 <nishant.nahata97@gmail.com>
2016-08-07 21:23:47 +0530

    FIREBASE ADDED with some changes

--- app/src/main/java/com/games/gamingmessenger/MainActivity.java
+++ app/src/main/java/com/games/gamingmessenger/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //TODO:COMPLETE THIS APP GUYS :P
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
