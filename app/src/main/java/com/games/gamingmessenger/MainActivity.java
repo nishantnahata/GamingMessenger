23f1be5d5032ea50835fe9a34f19c12622e6a5f7
nishantcoder97 <nishant.nahata97@gmail.com>
2016-08-07 21:40:29 +0530

    TODOS ADDED

--- app/src/main/java/com/games/gamingmessenger/MainActivity.java
+++ app/src/main/java/com/games/gamingmessenger/MainActivity.java
@@ -6,6 +6,15 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //TODO:COMPLETE THIS APP GUYS :P
+
+    //TODO: LOGIN ACTIVITY
+
+    //TODO: USER'S FRIENDLIST LIST ACTIVITY
+
+    //TODO: CHAT
+
+    //TODO: GAMES - PENALTY SHOOT OUT, JUGGLING, TIC TAC TOE,BASKETBALL AND SOME MORE GAMES...
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
